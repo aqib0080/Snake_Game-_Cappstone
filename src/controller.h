663c06fb46c2aa9c1fbb8d4def33d306d8edcd83
aqib0080 <syed_aqib@outlook.com>
2021-02-03 12:52:50 +0000

    Code for Capstone

---
+++ src/controller.h
@@ -0,0 +1,17 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "snake.h"
+
+class Controller {
+ public:
+  Controller(){}
+  ~Controller(){}
+  void HandleInput(bool &running, Snake &snake) const;
+
+ private:
+  void ChangeDirection(Snake &snake, Snake::Direction input,
+                       Snake::Direction opposite) const;
+};
+
+#endif
